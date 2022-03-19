077f795c490867bf15bd87692b89141578b704b5
Burtsev Alexander <Alexande.burzev@gmail.com>
2022-03-19 10:09:40 +0200

    fix(route): web

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Domains\Feedback\Http\Controllers\Api\FeedbackController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
  * Global Routes
@@ -11,13 +10,6 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
  * Routes that are used between both frontend and backend.
  */
 
-Route::group([
-    'prefix' => LaravelLocalization::setLocale(),
-    //'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'ip']
-], function () {
-
-    Route::get('/', [HomeController::class, 'index'])->name('index');
-    Route::post('/feedback/', [FeedbackController::class, "save"])->name("feedback.save");
-    Route::get('/feedback/', [FeedbackController::class, "index"])->name("feedback.form");
-     
-});
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::post('/feedback/', [FeedbackController::class, "save"])->name("feedback.save");
+Route::get('/feedback/', [FeedbackController::class, "index"])->name("feedback.form");
