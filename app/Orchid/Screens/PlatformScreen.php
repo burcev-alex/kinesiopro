192cf5c6152f44e35a40572a0f763792cf515888
Burtsev Alexander <Alexande.burzev@gmail.com>
2022-04-06 11:26:51 +0300

    feat(dashborad): statistic

--- app/Orchid/Screens/PlatformScreen.php
+++ app/Orchid/Screens/PlatformScreen.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Orchid\Screens;
 
+use App\Domains\Order\Models\Interfaces\OrderInterface;
+use App\Domains\Order\Models\Order;
+use App\Domains\User\Models\User;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Screen;
 use Orchid\Support\Facades\Layout;
@@ -31,7 +34,43 @@ class PlatformScreen extends Screen
      */
     public function query(): array
     {
-        return [];
+        $totalOrders = Order::where('payment_status', 'payed')->whereNotIn('state', [OrderInterface::STATE_CANCELLED])->sum('total');
+
+        $countPendingOrders = Order::where('payment_status', 'waiting')->whereNotIn('state', [OrderInterface::STATE_CANCELLED])->count();
+
+        $countUser = User::get()->count();
+
+        
+        $payCurrentMonth = (int) Order::where('payment_status', 'payed')
+            ->whereBetween('completed_at', [date('Y-m').'-01', date('Y-m').'-31'])
+            ->whereNotIn('state', [OrderInterface::STATE_CANCELLED])
+            ->sum('total');
+
+        $payPrevMonth = (int) Order::where('payment_status', 'payed')
+            ->whereBetween('completed_at', [date('Y-m', strtotime('-1 month')).'-01', date('Y-m', strtotime('-1 month')).'-31'])
+            ->whereNotIn('state', [OrderInterface::STATE_CANCELLED])
+            ->sum('total');
+
+        if($payPrevMonth > 0){
+            $percent = (($payCurrentMonth - $payPrevMonth) * 100) / (int) $payPrevMonth;
+        }
+        else{
+            if($payCurrentMonth > 0){
+                $percent = 100;
+            }
+            else{
+                $percent = 0;
+            }
+        }
+
+        return [
+            'metrics' => [
+                'Продажи, текущий месяц' => ['value' => number_format((int) $payCurrentMonth), 'diff' => round($percent, 2)],
+                'Кол-во пользователей' => ['value' => number_format((int) $countUser), 'diff' => 0],
+                'Не оплаченные заказы' => ['value' => number_format((int) $countPendingOrders), 'diff' => 0],
+                'Общая прибыть' => number_format((int)$totalOrders),
+            ],
+        ];
     }
 
     /**
--- resources/views/dashboard/welcome.blade.php
+++ resources/views/dashboard/welcome.blade.php
@@ -1,14 +1,18 @@
-<div class="admin-wrapper">
-    <div class="py-4">
-        <div class="row">
-            <div class="col-md-7 col-sm-12">
-                <div class="mb-5 jumbotron">
-                    <h1 class="display-5 text-dark font-thin m-b-sm">
-                        <i class="icon-trophy mr-2 mb-3"></i> Welcome!
-                    </h1>
-                    <p class="lead">Dashboard of Kinesio</p>
-                </div>
+<div class="row mb-2 g-3 g-mb-4">
+    @foreach($metrics as $key => $metric)
+        <div class="col">
+            <div class="p-4 bg-white rounded shadow-sm h-100">
+                <small class="text-muted d-block mb-1">{{ __($key) }}</small>
+                <p class="h3 text-black fw-light">
+                    {{ is_array($metric) ? $metric['value'] : $metric }}
+
+                    @if(isset($metric['diff']) && (float)$metric['diff'] !== 0.0)
+                        <small class="small {{ (float)$metric['diff'] < 0 ? 'text-danger': 'text-success' }}">
+                            {{ round($metric['diff'], 2) }} %
+                        </small>
+                    @endif
+                </p>
             </div>
         </div>
-    </div>
+    @endforeach
 </div>
